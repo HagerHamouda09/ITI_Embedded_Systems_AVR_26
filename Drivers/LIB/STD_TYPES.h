b43d0a7efc81d15bab7149d67dfcbc06bfdf780b
hagar03 <hagaradelhamouda@gmail.com>
2026-07-22 14:01:55 +0300

    Drivers

---
+++ Drivers/LIB/STD_TYPES.h
@@ -0,0 +1,42 @@
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+
+// 8 bits
+typedef unsigned char u8;
+typedef signed char s8;
+
+// 16 bits
+typedef unsigned short int u16;
+typedef signed short int s16;
+
+// 32 bits
+typedef unsigned long int u32;
+typedef signed long int s32;
+
+// 64 bits
+typedef unsigned long long int u64;
+
+// floating points
+typedef float f32;
+typedef double f64;
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
