737f1ec7a3a2a860a053997d99eb29dadbfd1442
hagar03 <hagaradelhamouda@gmail.com>
2026-07-29 19:19:34 +0300

    EXTI DRIVER

---
+++ Drivers/MCAL/GIE/GIE_int.h
@@ -0,0 +1,15 @@
+/*
+ * GIE_int.h
+ *
+ *  Created on: Jul 28, 2026
+ *      Author: Hager Adel
+ */
+
+#ifndef MCAL_GIE_GIE_INT_H_
+#define MCAL_GIE_GIE_INT_H_
+
+
+void MGIE_vEnableGlobalInterrupt(void);
+void MGIE_vDisableGlobalInterrupt(void);
+
+#endif /* MCAL_GIE_GIE_INT_H_ */
