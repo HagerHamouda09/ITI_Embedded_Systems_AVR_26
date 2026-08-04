d877b299b6a8eba74b41be1b7e0913ae01d404ab
Hager Adel <115931896+HagerHamouda09@users.noreply.github.com>
2026-08-04 21:09:40 +0300

    Update TMR_prv.h

--- Drivers/MCAL/TMR/TMR_prv.h
+++ Drivers/MCAL/TMR/TMR_prv.h
@@ -8,11 +8,21 @@
 #ifndef MCAL_TMR_TMR_PRV_H_
 #define MCAL_TMR_TMR_PRV_H_
 
+//TIMER0
 #define OCR0	*((volatile u8*)(0x5C))
 #define TIMSK	*((volatile u8*)(0x59))
 #define TCCR0	*((volatile u8*)(0x53))
 #define TCNT0	*((volatile u8*)(0x52))
 #define TIFR	*((volatile u8*)(0x58))
+
+// TIMER1
+#define TCCR1A	*((volatile u8*)(0x4F))
+#define TCCR1B	*((volatile u8*)(0x4E))
+#define TCNT1	*((volatile u16*)(0x4C))
+#define OCR1A	*((volatile u16*)(0x4A))
+#define OCR1B	*((volatile u16*)(0x48))
+#define ICR1	*((volatile u16*)(0x46))
+
 
 //TCCR0 BITS
 #define	FOC0	7
